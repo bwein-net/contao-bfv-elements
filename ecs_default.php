757948126f62efb05d25996eac818036c9b8d0c8
Ralf Baussenwein <ralf@bwein.net>
2021-03-08 01:07:50 +0100

    Update ecs config

---
+++ ecs_default.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use SlevomatCodingStandard\Sniffs\TypeHints\DisallowArrayTypeHintSyntaxSniff;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $vendorDir = __DIR__.'/vendor';
+
+    if (!is_dir($vendorDir)) {
+        $vendorDir = __DIR__.'/../../vendor';
+    }
+
+    $containerConfigurator->import($vendorDir.'/contao/easy-coding-standard/config/default.php');
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SKIP, [
+        DisallowArrayTypeHintSyntaxSniff::class => [
+            '*Model.php',
+        ],
+    ]);
+
+    $parameters->set(Option::EXCLUDE_PATHS, ['*/templates/*']);
+    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_default_cache_contact');
+
+    $services = $containerConfigurator->services();
+    $services
+        ->set(HeaderCommentFixer::class)
+        ->call('configure', [[
+            'header' => "This file is part of BFV Elements for Contao Open Source CMS.\n\n(c) bwein.net\n\n@license MIT",
+        ]])
+    ;
+};
